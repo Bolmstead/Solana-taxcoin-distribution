aebba159c2b545a6fca0c2cb7c04576ed036b3f0
BerkleyDEV <88558484+BerkleyDEV@users.noreply.github.com>
2025-03-14 13:31:49 -0600

    pnpm execute updated

--- src/index.js
+++
@@ -1,8 +0,0 @@
-require("dotenv").config();
-const cron = require("node-cron");
-const { execute } = require("./helpers/tokenHolders");
-// Schedule the distribution to run every 5 minutes
-cron.schedule("*/1 * * * *", async () => {
-  console.log("Running scheduled distribution...");
-  await execute();
-});
